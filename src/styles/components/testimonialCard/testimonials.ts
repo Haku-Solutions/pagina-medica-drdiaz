b5bf33d01cd96586c7277f9ddd95da1c1f55c732
Karime Casas <monseee.ca@gmail.com>
2026-07-18 17:59:47 -0600

    feat: add authentic testimonials

--- src/styles/components/testimonialCard/testimonials.ts
+++ src/styles/components/testimonialCard/testimonials.ts
@@ -6,13 +6,13 @@ export interface Testimonial {
 
 export const testimonials: Testimonial[] = [
   {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    author: "John Doe",
+    text: "El doctor Díaz es un profesional cirujano Te brinda una atención clara, honesta te ayuda en todo momento.. El y sus enfermeras dan un excelente servicio..",
+    author: "Anayely Geronimo Salvador, san Francisco municipio de tecuanapa",
     variant: "blue",
   },
   {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    author: "John Doe",
+    text: "Recomiendo ampliamente al Dr. Díaz. En mi experiencia, es un profesional muy atento y comprometido con sus pacientes. Siempre brinda un excelente trato, explica todo con claridad y transmite mucha confianza. Sin duda, lo volvería a recomendar.",
+    author: "Paty Prestegui, Cruz Grande",
     variant: "white",
   },
   {
